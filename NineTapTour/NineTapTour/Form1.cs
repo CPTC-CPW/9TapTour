b95a2a776bd8d635063ef11da6232fd518c3175d
AlecasaurusRex <imalecb@hotmail.com>
2015-04-27 13:17:12 -0700

    9 tap interface

--- NineTapTour/NineTapTour/Form1.cs
+++ NineTapTour/NineTapTour/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace NineTapTour
 {
-    public partial class Form1 : Form
+    public partial class frmMemberData : Form
     {
-        public Form1()
+        public frmMemberData()
         {
             InitializeComponent();
+        }
+
+        private void MemberDataForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
