f5cf60538aa074095fe55821133287248c37ae5c
Roger-Dogood <daftdahl@gmail.com>
2015-04-27 14:12:59 -0700

    Add member model

---
+++ NineTapTour/NineTapTour/Database/Member.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NineTapTour.Database
+{
+    public class Member
+    {
+        [Key]
+        public int MemberNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleInitial { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public MemberGenders Gender { get; set; }
+        public bool IsSenior { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime JoinDate { get; set; }
+        public int Referals { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string PrimaryPhone { get; set; }
+        public string SecondaryPhone { get; set; }
+
+    }
+
+    public enum MemberGenders
+    {
+        Female,
+        Male
+    }
+}
