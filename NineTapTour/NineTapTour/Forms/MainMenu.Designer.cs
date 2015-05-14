0b29f9a83cb78aaaff6123ceaf10e474cbdb3e6a
imalecb@hotmail.com <alec95balles3110>
2015-05-14 12:29:27 -0700

    Added main menu

---
+++ NineTapTour/NineTapTour/Forms/MainMenu.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace NineTapTour.Forms
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMemberData = new System.Windows.Forms.Button();
+            this.btnMemberScores = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMemberData
+            // 
+            this.btnMemberData.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMemberData.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnMemberData.Location = new System.Drawing.Point(44, 347);
+            this.btnMemberData.Name = "btnMemberData";
+            this.btnMemberData.Size = new System.Drawing.Size(118, 70);
+            this.btnMemberData.TabIndex = 0;
+            this.btnMemberData.Text = "Memeber Data";
+            this.btnMemberData.UseVisualStyleBackColor = true;
+            this.btnMemberData.Click += new System.EventHandler(this.btnMemberData_Click);
+            // 
+            // btnMemberScores
+            // 
+            this.btnMemberScores.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMemberScores.Location = new System.Drawing.Point(44, 488);
+            this.btnMemberScores.Name = "btnMemberScores";
+            this.btnMemberScores.Size = new System.Drawing.Size(118, 71);
+            this.btnMemberScores.TabIndex = 0;
+            this.btnMemberScores.Text = "Member Scores";
+            this.btnMemberScores.UseVisualStyleBackColor = true;
+            this.btnMemberScores.Click += new System.EventHandler(this.btnMemberScores_Click);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbout.Location = new System.Drawing.Point(632, 347);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(118, 71);
+            this.btnAbout.TabIndex = 0;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.Location = new System.Drawing.Point(632, 488);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(118, 71);
+            this.Exit.TabIndex = 0;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BackgroundImage = global::NineTapTour.Properties.Resources._9tap;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(796, 665);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnMemberScores);
+            this.Controls.Add(this.btnMemberData);
+            this.DoubleBuffered = true;
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMemberData;
+        private System.Windows.Forms.Button btnMemberScores;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button Exit;
+    }
+}
