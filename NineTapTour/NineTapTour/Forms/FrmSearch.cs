97afcda14398a68ad56ab753d4ac948ceac4fa9d
MichelleGoreczny <mng17213@msn.com>
2015-12-09 13:06:32 -0800

    Added comments to the "Search" form code.

--- NineTapTour/NineTapTour/Forms/FrmSearch.cs
+++ NineTapTour/NineTapTour/Forms/FrmSearch.cs
@@ -12,11 +12,18 @@ namespace NineTapTour.Forms
 {
     public partial class FrmSearch : Form
     {
+        /// <summary>
+        /// Opens the "Search" form.
+        /// </summary>
         public FrmSearch()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Closes the "Search" form without doing anything.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnCancel_Click(object sender, EventArgs e)
         {
             this.Close();
