c5141db2a48c761abcfa40236235205ad656e929
Kevin Carbonell <famouskevinc@gmail.com>
2016-05-17 13:09:45 -0700

    Added a print button for stats

--- NineTapTour/NineTapTour/Forms/FrmStats.Designer.cs
+++ NineTapTour/NineTapTour/Forms/FrmStats.Designer.cs
@@ -34,6 +34,7 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.lblName = new System.Windows.Forms.Label();
             this.lblMemberNumber = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.nineTapTourNineTapDbDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this._NineTapTour_NineTapDbDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -53,18 +54,20 @@
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(83, 85);
+            this.dataGridView1.Location = new System.Drawing.Point(221, 203);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1036, 722);
+            this.dataGridView1.Size = new System.Drawing.Size(2763, 1722);
             this.dataGridView1.TabIndex = 3;
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(217, 30);
+            this.lblName.Location = new System.Drawing.Point(579, 72);
+            this.lblName.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(87, 36);
+            this.lblName.Size = new System.Drawing.Size(218, 90);
             this.lblName.TabIndex = 4;
             this.lblName.Text = "Name";
             // 
@@ -72,20 +75,32 @@
             // 
             this.lblMemberNumber.AutoSize = true;
             this.lblMemberNumber.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMemberNumber.Location = new System.Drawing.Point(77, 30);
+            this.lblMemberNumber.Location = new System.Drawing.Point(205, 72);
+            this.lblMemberNumber.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.lblMemberNumber.Name = "lblMemberNumber";
-            this.lblMemberNumber.Size = new System.Drawing.Size(134, 36);
+            this.lblMemberNumber.Size = new System.Drawing.Size(336, 90);
             this.lblMemberNumber.TabIndex = 5;
             this.lblMemberNumber.Text = "Member#";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2624, 1726);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(321, 166);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FrmStats
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1158, 849);
+            this.ClientSize = new System.Drawing.Size(3088, 2025);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblMemberNumber);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "FrmStats";
             this.Text = "FrmStats";
             this.Load += new System.EventHandler(this.FrmStats_Load);
@@ -104,5 +119,6 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Label lblMemberNumber;
+        private System.Windows.Forms.Button button1;
     }
 }
