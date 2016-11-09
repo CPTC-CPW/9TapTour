57c29102d73ea82385b4921b3a91c3c143f03b1c
Chris Rocks <cougrocks@gmail.com>
2016-11-09 09:42:14 -0800

    frmfinalize test

--- NineTapTour/NineTapTour/Forms/FrmFinalizeTournament.cs
+++ NineTapTour/NineTapTour/Forms/FrmFinalizeTournament.cs
@@ -16,5 +16,34 @@ namespace NineTapTour.Forms
         {
             InitializeComponent();
         }
+        public DataTable DataView()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Last Name");
+            dt.Columns.Add("First Name");
+            dt.Columns.Add("Game 1");
+            dt.Columns.Add(new DataColumn("Valid Score?", typeof(bool)));
+            dt.Columns.Add("Game 2");
+            dt.Columns.Add(new DataColumn("Valid Score?", typeof(bool)));
+            dt.Columns.Add("Game 3");
+            dt.Columns.Add(new DataColumn("Valid Score?", typeof(bool)));
+            dt.Columns.Add("Game 4");
+            dt.Columns.Add(new DataColumn("Valid Score?", typeof(bool)));
+            dt.Columns.Add("True Avg");
+            dt.Columns.Add("Adjusted Avg");
+            dt.Columns.Add(new DataColumn("Keep True Avg?", typeof(bool)));
+            dt.Columns.Add("Scratch Total");
+            dt.Columns.Add("Date");
+            dt.Columns.Add("Date");
+            dt.Columns.Add("Date");
+            dt.Columns.Add("Date");
+            dt.Columns.Add("Date");
+            dt.Columns.Add("Date");
+            dt.Columns.Add("Date");
+            dt.Columns.Add("Date");
+
+
+            return dt;
+        }
     }
 }
