2eb461a830c96833319d420c4fb77c8aa1cd6750
dbasargin <dennisbasargin@hotmail.com>
2016-11-11 00:50:03 -0800

    Added Methods to sort , organize and help display Participants properly in Finalize tournament Form- dennis

--- NineTapTour/NineTapTour/Forms/FrmFinalizeTournament.cs
+++ NineTapTour/NineTapTour/Forms/FrmFinalizeTournament.cs
@@ -13,11 +13,13 @@ namespace NineTapTour.Forms
 {
     public partial class FrmFinalizeTournament : Form
     {
+
         public FrmFinalizeTournament(Tournament tourn)
         {
             Tournament temptourn = tourn;
             InitializeComponent();
-            dataGridView1.DataSource = DataView(temptourn);
+            dataGridView1.DataSource = DataView(temptourn, GetAllParticipantGameList(tourn)); //By default populates all datagrid with all participant for tournament.
+            
             //sets sizes of check box columns "Valid Score1, ValidScore2, ValidScore3, Valid Score 4, and Keep True Avg?"
             var column = dataGridView1.Columns[2];
             column.Width = 50;
@@ -31,8 +33,10 @@ namespace NineTapTour.Forms
             column4.Width = 40;
 
         }
+
+
         //creates the dataview that will populate the datagridview table on form
-        public DataTable DataView(Tournament tourn)
+        public DataTable DataView(Tournament tourn, List<GameParticipant> participantsList)
         {
             var db = new NineTapDb();
             DataTable dt = new DataTable();
@@ -56,26 +60,10 @@ namespace NineTapTour.Forms
             dt.Columns.Add("Bonus");
             dt.Columns.Add("Pro Pot");
             dt.Columns.Add("Notes");
-            var temp = (from p in db.Participants
-                        join m in db.Members on p.Member.Id equals m.Id
-                        join g in db.Games on p.Game.Id equals g.Id
-                        join t in db.Tournaments on p.Tournament.Id equals t.Id
-                        where tourn.Id == p.Tournament.Id
-                        orderby m.FirstName descending
-                        select new
-                        {
-                            m.FirstName,
-                            m.LastName,
-                            p.Squad,
-                            g.Game1,
-                            g.Game2,
-                            g.Game3,
-                            g.Game4,                           
-                            g.Handicap,
-                            g.Bonus,
-                            
-                            
-                        }).ToList();
+
+            //whatever list of participants you pass into method will be populated into grid
+            List<GameParticipant> temp = participantsList;
+
             //loops thru each person's info in tournament and populates the dataview with data from DB.
             foreach (var item in temp)
             {
@@ -106,5 +94,128 @@ namespace NineTapTour.Forms
 
             return dt;
         }
+
+        /// <summary>
+        /// Gets list of Game Participants by squad
+        /// </summary>
+        /// <param name="tourn">active tournament</param>
+        /// <param name="Squad">squad you want a list of </param>
+        /// <returns>returns list of Participants from specified squad</returns>
+        public List<GameParticipant> GameParticipantsBySquadList(Tournament tourn, int Squad)
+        {
+            List<GameParticipant> allParticipants = ParticipantSortByScore(tourn);
+            List<GameParticipant> SquadParticipants = new List<GameParticipant>();
+            foreach (GameParticipant p in allParticipants)
+            {
+                if(p.Squad == Squad)
+                {
+                    SquadParticipants.Add(p);
+                }
+            }
+            return SquadParticipants;
+        }
+
+        /// <summary>
+        /// THis method Gets a list of all participant objects for the tournament passed into method.
+        /// </summary>
+        /// <param name="tourn"> represent the tournament you want list of particpants from</param>
+        /// <returns>List of Participants for specific tournament</returns>
+        public List<GameParticipant> GetAllParticipantGameList(Tournament tourn)
+        {
+            var db = new NineTapDb();
+            List<GameParticipant> ParticipantList = new List<GameParticipant>();
+            var temp = (from p in db.Participants
+                        join m in db.Members on p.Member.Id equals m.Id
+                        join g in db.Games on p.Game.Id equals g.Id
+                        join t in db.Tournaments on p.Tournament.Id equals t.Id
+                        where tourn.Id == p.Tournament.Id
+                        orderby m.FirstName descending
+                        select new
+                        {
+                            m.FirstName,
+                            m.LastName,
+                            p.Squad,
+                            g.Game1,
+                            g.Game2,
+                            g.Game3,
+                            g.Game4,
+                            g.Handicap,
+                            g.Bonus,
+
+
+                        }).ToList();
+            foreach (var item in temp)
+            {
+                GameParticipant NewParticipant = new GameParticipant();
+                NewParticipant.FirstName = item.FirstName;
+                NewParticipant.LastName = item.LastName;
+                NewParticipant.Game1 = (int)item.Game1;
+                NewParticipant.Game2 = (int) item.Game2;
+                NewParticipant.Game3 = (int) item.Game3;
+                NewParticipant.Game4 = (int) item.Game4;
+                //TODO: Base this off historical records for member off their last 30 games.
+                NewParticipant.ScratchTotal = (int) (item.Game1 + item.Game2 + item.Game3 + item.Game4);
+                NewParticipant.Squad = item.Squad;
+                NewParticipant.GameAvg = (int) (item.Game1 + item.Game2 + item.Game3 + item.Game4) / 4;
+                NewParticipant.Handicap = (int) item.Handicap;
+                NewParticipant.Bonus =(int)item.Bonus;
+                ParticipantList.Add(NewParticipant);
+            }
+            return ParticipantList;
+        }
+        
+        /// <summary>
+        /// This method will get a list of all tournament participants and return a sort the list by scores.
+        /// </summary>
+        /// <param name="tourn">Tournament needing information from</param>
+        /// <returns>sorted list of gameParticipants for specified tournament</returns>
+        public List<GameParticipant> ParticipantSortByScore(Tournament tourn)
+        {
+            List<GameParticipant> sortParticipant = GetAllParticipantGameList(tourn);
+            sortParticipant.Sort(delegate (GameParticipant c1, GameParticipant c2) { return c1.GameAvg.CompareTo(c2.GameAvg); });
+            return sortParticipant;
+        }
+
+        /// <summary>
+        /// gets list of Winning Participants
+        /// </summary>
+        /// <param name="tourn">active tournament</param>
+        /// <returns>returns list of Top Scoring Participants</returns>
+        public List<GameParticipant> TopScoreParticipants(Tournament tourn)
+        {
+            //This Method needs some work... to get the right list but more or less is the basic design.
+            List<GameParticipant> SortedParticipantsByScore = ParticipantSortByScore(tourn);
+            List<GameParticipant> TopParticipantsList = new List<GameParticipant>();
+
+            //reverse instantiation if my sort method is backwards and 
+            // add business rules applied to determine correct list of winners.
+            for (int i = 0; i == 4; i++)
+            {
+                TopParticipantsList.Add(SortedParticipantsByScore[i]);
+            }
+            return TopParticipantsList;
+        }
+
+
+        /// <summary>
+        /// This Class represents an object of Participant Info for a specific game.
+        /// </summary>
+        public class GameParticipant {
+
+            public String FirstName { get; set; }
+            public String LastName { get; set; }
+            public int Squad { get; set; }
+            public int Game1 { get; set; }
+            public int Game2 { get; set; }
+            public int Game3 { get; set; }
+            public int Game4 { get; set; }
+            public int ScratchTotal { get; set; }
+            public int GameAvg { get; set; }
+            public int Handicap { get; set; }
+            public int Bonus { get; set; }
+
+        }
+
+
     }
 }
