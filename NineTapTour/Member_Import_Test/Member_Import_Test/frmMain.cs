6b4b0d07fc51901782efb96cd0b92545c11bd705
John Chandler <chandler6830@student.cptc.edu>
2017-06-05 11:32:58 -0700

    Made the Excel Processing able to import from multipal folders and added a progress bar to the excel prcessing

--- NineTapTour/Member_Import_Test/Member_Import_Test/frmMain.cs
+++ NineTapTour/Member_Import_Test/Member_Import_Test/frmMain.cs
@@ -471,10 +471,20 @@ namespace Member_Import_Test
 
         private void button1_Click(object sender, EventArgs e)
         {
+            GetAndProcessFolderWithExcelFiles();
+            while (MessageBox.Show("Do You have more Excel Files to import?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                GetAndProcessFolderWithExcelFiles();
+            }
+        }
+
+        private void GetAndProcessFolderWithExcelFiles()
+        {
             using (var fbd = new FolderBrowserDialog())
             {
                 DialogResult result = fbd.ShowDialog();
-                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath)) {
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
                     string[] files = Directory.GetFiles(fbd.SelectedPath);
                     GetAllExcelData(files);
                 }
@@ -482,8 +492,12 @@ namespace Member_Import_Test
             checkSpaces();
         }
 
-        private static List<ExcelRow> GetAllExcelData(string[] files)
+        private List<ExcelRow> GetAllExcelData(string[] files)
         {
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = files.Length;
+            progressBar1.Value = 0;
+
             for (int i = 0; i < files.Length; i++)
             {
                 if (Path.GetExtension(files[i]) != ".xls")
@@ -496,8 +510,8 @@ namespace Member_Import_Test
                 {
                     ALLEXCELDATAFROMALLPLAYERS.Add(r);
                 }
+                progressBar1.Increment(i);
             }
-            MessageBox.Show("All of the Member Excel Files have been Imported.");
             return ALLEXCELDATAFROMALLPLAYERS;
         }
 
@@ -829,6 +843,11 @@ namespace Member_Import_Test
                 TournamentDb.AddTournament(t);
             }
         }
+
+        //private void progressBar1_Click(object sender, EventArgs e)
+        //{
+
+        //}
     }
 }
 
