8bc8de53e68f8412a02d0f0db7a7598777fc28cd
Dorothy Dean <Dorothy@DESKTOP-L7PIFDI>
2018-03-19 15:50:32 -0700

    Fixed a weird merge mistake

--- NineTapTour/NineTapTour/Forms/FrmMemberScores.cs
+++ NineTapTour/NineTapTour/Forms/FrmMemberScores.cs
@@ -2878,15 +2878,87 @@ namespace NineTapTour.Forms
             RecordIndexOnSquadSwitch(total);
             FillMember();
         }
-    }
 
+		private void txtMemberNum2_Leave(object sender, EventArgs e)
+		{
+			FillMember();
+		}
 
+		/// <summary>
+		/// The resize event for the form
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void frmMemberScores_Resize(object sender, EventArgs e)
+		{
+			SetFlowDirection(flpMemberScores);
+		}
 
+		/// <summary>
+		/// Sets the flow direction for the flowlayoutpanel depending
+		/// on the pixel width or height of the screen.
+		/// </summary>
+		/// <param name="flp">The flowlayoutpanel that is being passed in to have changes made to it.</param>
+		private void SetFlowDirection(FlowLayoutPanel flp)
+		{
+			if (Convert.ToInt32(Form.ActiveForm.Size.Width) > 1100 && Convert.ToInt32(Form.ActiveForm.Size.Height) < 766)
+			{
+				flp.FlowDirection = FlowDirection.TopDown;
+			}
+			else
+			{
+				flp.FlowDirection = FlowDirection.LeftToRight;
+			}
+		}
 
+		/// <summary>
+		/// After the size of the form has been changed, it checks the pixel
+		/// width and height to determine whether there needs to be scroll bars
+		/// or not.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void flpMemberScores_SizeChanged(object sender, EventArgs e)
+		{
+			SetFlowControlScrollBars(flpMemberScores);
+		}
 
+		/// <summary>
+		/// Sets whether the scroll bars are enabled or disabled
+		/// and whether you can see them or not depending on the
+		/// pixel width or height of the screen.
+		/// </summary>
+		/// <param name="flp">The flowlayoutpanel that is being passed in to have changes made to it.</param>
+		private void SetFlowControlScrollBars(FlowLayoutPanel flp)
+		{
+			if (Convert.ToInt32(Form.ActiveForm.Size.Width) < 1300)
+			{
+				flpMemberScores.HorizontalScroll.Visible = true;
+				flpMemberScores.HorizontalScroll.Enabled = true;
+			}
+			if (Convert.ToInt32(Form.ActiveForm.Size.Height) < 750)
+			{
+				flpMemberScores.VerticalScroll.Visible = true;
+				flpMemberScores.VerticalScroll.Enabled = true;
+			}
+			if (Convert.ToInt32(Form.ActiveForm.Size.Width) > 1300)
+			{
+				flpMemberScores.HorizontalScroll.Visible = false;
+				flpMemberScores.HorizontalScroll.Enabled = false;
+			}
+			if (Convert.ToInt32(Form.ActiveForm.Size.Height) > 750)
+			{
+				flpMemberScores.VerticalScroll.Visible = false;
+				flpMemberScores.VerticalScroll.Enabled = false;
+			}
+		}
+		//runs fill member when you tab out of text box
+		private void txtMemberNum_Leave(object sender, EventArgs e)
+		{
+			FillMember();
+		}
+	}
 
-
-    }
     /// <summary>
     /// Class used to populate 3rd RichTextBox
     /// </summary>
@@ -2915,6 +2987,7 @@ namespace NineTapTour.Forms
         public int GameID { get; set; }
         #endregion
     }
+
 }
 
 
