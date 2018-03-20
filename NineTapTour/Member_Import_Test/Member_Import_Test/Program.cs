3649ea60d66ca27bcecda6a35816a3f3b3a5a174
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2018-03-20 14:04:34 -0700

    Created MemberImport installer and added to bootstrapper. Added Debug/Release connection strings for MemberImport project

--- NineTapTour/Member_Import_Test/Member_Import_Test/Program.cs
+++ NineTapTour/Member_Import_Test/Member_Import_Test/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,10 +14,26 @@ namespace Member_Import_Test
         /// </summary>
         [STAThread]
         static void Main()
-       {
+        {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+
+#if !DEBUG //if app is set to release mode
+            SetConnectionString(@".\SQLExpress");
+#elif DEBUG //set connection to dev database
+            SetConnectionString(@"(localdb)\MSSQLLocalDB");
+#endif
+
             Application.Run(new frmMain());
+        }
+
+        private static void SetConnectionString(string dataSource)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.ConnectionStrings.ConnectionStrings["NineTapDbConnection"].ConnectionString = String.Format("data source={0};initial catalog=NineTapTour.NineTapDb;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework", dataSource);
+            config.Save(ConfigurationSaveMode.Modified, true);
+            ConfigurationManager.RefreshSection("connectionStrings");
         }
     }
 }
