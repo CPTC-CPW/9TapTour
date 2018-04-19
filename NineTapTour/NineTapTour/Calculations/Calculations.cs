c56001aecf2bdf80e2a4cc98f5d3e6fab82f2195
Joe <joeprogrammer88@users.noreply.github.com>
2018-04-18 23:08:21 -0700

    230 average should get handicap set to -8

--- NineTapTour/NineTapTour/Calculations/Calculations.cs
+++ NineTapTour/NineTapTour/Calculations/Calculations.cs
@@ -39,6 +39,12 @@ namespace NineTapTour.Calculations
         {
 
             double calculateHandicap = Convert.ToDouble(BASE_AVERAGE_HANDICAP_CALCULATOR - currentAverage) * PERCENTAGE_TO_CALCULATE_HANDICAP;
+            //230 bowling average should be set to -8 handicap regardless of math
+            if (currentAverage == 230)
+            {
+                calculateHandicap = -8;
+                return (int)calculateHandicap;
+            }
             if (calculateHandicap > MAX_HANDICAP_PINS)
             {
                 return MAX_HANDICAP_PINS;
