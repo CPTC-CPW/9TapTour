39d7fc15ccd32ec61d49a288f020b2f767af06bd
Joe <joeprogrammer88@users.noreply.github.com>
2018-04-18 23:10:48 -0700

    Changed location of squad check. Small refactoring

--- NineTapTour/NineTapTour/Forms/FrmMemberScores.Designer.cs
+++ NineTapTour/NineTapTour/Forms/FrmMemberScores.Designer.cs
@@ -82,7 +82,7 @@
             this.cbxTourneyDropDown = new System.Windows.Forms.ComboBox();
             this.grpLeaders = new System.Windows.Forms.GroupBox();
             this.QBSBOX = new System.Windows.Forms.GroupBox();
-            this.rdoSquad8Resualts = new System.Windows.Forms.RadioButton();
+            this.rdoSquad8Results = new System.Windows.Forms.RadioButton();
             this.rdoSquad7Results = new System.Windows.Forms.RadioButton();
             this.rdoSquad6Results = new System.Windows.Forms.RadioButton();
             this.rdoSquad5Results = new System.Windows.Forms.RadioButton();
@@ -125,9 +125,9 @@
             this.btnTournamentResults = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.flpMemberScores = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.grpMemberNum.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -142,9 +142,9 @@
             this.pnlMemStat.SuspendLayout();
             this.panel1.SuspendLayout();
             this.flpMemberScores.SuspendLayout();
-            this.panel3.SuspendLayout();
             this.panel5.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -209,6 +209,7 @@
             this.rdoSquad8.TabStop = true;
             this.rdoSquad8.Text = "Squad 8";
             this.rdoSquad8.UseVisualStyleBackColor = true;
+            this.rdoSquad8.CheckedChanged += new System.EventHandler(this.rdoSquadNumber_CheckedChanged);
             // 
             // rdoSquad6
             // 
@@ -220,6 +221,7 @@
             this.rdoSquad6.TabStop = true;
             this.rdoSquad6.Text = "Squad 6";
             this.rdoSquad6.UseVisualStyleBackColor = true;
+            this.rdoSquad6.CheckedChanged += new System.EventHandler(this.rdoSquadNumber_CheckedChanged);
             // 
             // rdoSquad7
             // 
@@ -231,6 +233,7 @@
             this.rdoSquad7.TabStop = true;
             this.rdoSquad7.Text = "Squad 7";
             this.rdoSquad7.UseVisualStyleBackColor = true;
+            this.rdoSquad7.CheckedChanged += new System.EventHandler(this.rdoSquadNumber_CheckedChanged);
             // 
             // rdoSquad5
             // 
@@ -242,6 +245,7 @@
             this.rdoSquad5.TabStop = true;
             this.rdoSquad5.Text = "Squad 5";
             this.rdoSquad5.UseVisualStyleBackColor = true;
+            this.rdoSquad5.CheckedChanged += new System.EventHandler(this.rdoSquadNumber_CheckedChanged);
             // 
             // rdoSquadFour
             // 
@@ -252,6 +256,7 @@
             this.rdoSquadFour.TabIndex = 23;
             this.rdoSquadFour.Text = "Squad 4";
             this.rdoSquadFour.UseVisualStyleBackColor = true;
+            this.rdoSquadFour.CheckedChanged += new System.EventHandler(this.rdoSquadNumber_CheckedChanged);
             // 
             // rdoSquadThree
             // 
@@ -262,6 +267,7 @@
             this.rdoSquadThree.TabIndex = 22;
             this.rdoSquadThree.Text = "Squad 3";
             this.rdoSquadThree.UseVisualStyleBackColor = true;
+            this.rdoSquadThree.CheckedChanged += new System.EventHandler(this.rdoSquadNumber_CheckedChanged);
             // 
             // rdoSquadTwo
             // 
@@ -272,6 +278,7 @@
             this.rdoSquadTwo.TabIndex = 21;
             this.rdoSquadTwo.Text = "Squad 2";
             this.rdoSquadTwo.UseVisualStyleBackColor = true;
+            this.rdoSquadTwo.CheckedChanged += new System.EventHandler(this.rdoSquadNumber_CheckedChanged);
             // 
             // rdoSquadOne
             // 
@@ -284,6 +291,7 @@
             this.rdoSquadOne.TabStop = true;
             this.rdoSquadOne.Text = "Squad 1";
             this.rdoSquadOne.UseVisualStyleBackColor = true;
+            this.rdoSquadOne.CheckedChanged += new System.EventHandler(this.rdoSquadNumber_CheckedChanged);
             // 
             // txtLastName
             // 
@@ -768,7 +776,7 @@
             // 
             // QBSBOX
             // 
-            this.QBSBOX.Controls.Add(this.rdoSquad8Resualts);
+            this.QBSBOX.Controls.Add(this.rdoSquad8Results);
             this.QBSBOX.Controls.Add(this.rdoSquad7Results);
             this.QBSBOX.Controls.Add(this.rdoSquad6Results);
             this.QBSBOX.Controls.Add(this.rdoSquad5Results);
@@ -784,16 +792,16 @@
             this.QBSBOX.TabStop = false;
             this.QBSBOX.Text = "Qualify By Squad";
             // 
-            // rdoSquad8Resualts
+            // rdoSquad8Results
             // 
-            this.rdoSquad8Resualts.AutoSize = true;
-            this.rdoSquad8Resualts.Location = new System.Drawing.Point(4, 230);
-            this.rdoSquad8Resualts.Name = "rdoSquad8Resualts";
-            this.rdoSquad8Resualts.Size = new System.Drawing.Size(65, 17);
-            this.rdoSquad8Resualts.TabIndex = 8;
-            this.rdoSquad8Resualts.Text = "Squad 8";
-            this.rdoSquad8Resualts.UseVisualStyleBackColor = true;
-            this.rdoSquad8Resualts.CheckedChanged += new System.EventHandler(this.rdoSquad8Resualts_CheckedChanged);
+            this.rdoSquad8Results.AutoSize = true;
+            this.rdoSquad8Results.Location = new System.Drawing.Point(4, 230);
+            this.rdoSquad8Results.Name = "rdoSquad8Results";
+            this.rdoSquad8Results.Size = new System.Drawing.Size(65, 17);
+            this.rdoSquad8Results.TabIndex = 8;
+            this.rdoSquad8Results.Text = "Squad 8";
+            this.rdoSquad8Results.UseVisualStyleBackColor = true;
+            this.rdoSquad8Results.CheckedChanged += new System.EventHandler(this.rdoSquad8Resualts_CheckedChanged);
             // 
             // rdoSquad7Results
             // 
@@ -1259,15 +1267,6 @@
             this.flpMemberScores.TabIndex = 1;
             this.flpMemberScores.SizeChanged += new System.EventHandler(this.flpMemberScores_SizeChanged);
             // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.grpReports);
-            this.panel3.Controls.Add(this.btnTournamentResults);
-            this.panel3.Location = new System.Drawing.Point(664, 550);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(139, 207);
-            this.panel3.TabIndex = 46;
-            // 
             // panel5
             // 
             this.panel5.Controls.Add(this.btnRecapByPin);
@@ -1288,6 +1287,15 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(206, 184);
             this.panel2.TabIndex = 46;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.grpReports);
+            this.panel3.Controls.Add(this.btnTournamentResults);
+            this.panel3.Location = new System.Drawing.Point(664, 550);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(139, 207);
+            this.panel3.TabIndex = 46;
             // 
             // panel4
             // 
@@ -1335,9 +1343,9 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.flpMemberScores.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
             this.panel5.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -1428,7 +1436,7 @@
         private System.Windows.Forms.TextBox txtMoney;
         private System.Windows.Forms.Button btnFinalizeTounament;
         private System.Windows.Forms.GroupBox QBSBOX;
-        private System.Windows.Forms.RadioButton rdoSquad8Resualts;
+        private System.Windows.Forms.RadioButton rdoSquad8Results;
         private System.Windows.Forms.RadioButton rdoSquad7Results;
         private System.Windows.Forms.RadioButton rdoSquad6Results;
         private System.Windows.Forms.RadioButton rdoSquad5Results;
