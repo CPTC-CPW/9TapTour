f27bb5bc1ea9f095ecf5d2a523371e28312042a2
taylor nickle <taylor-nickle1@hotmail.com>
2018-06-12 10:30:14 -0700

    Added MessageBox to FrmSearch form that notifies a user if their query had no results

--- NineTapTour/NineTapTour/Forms/FrmSearch.cs
+++ NineTapTour/NineTapTour/Forms/FrmSearch.cs
@@ -181,6 +181,7 @@ namespace NineTapTour.Forms
             else
             {
                 EmptyGrid();
+                MessageBox.Show("There are no bowlers with that Information");
             }
             btnSelect.Focus();
         }
