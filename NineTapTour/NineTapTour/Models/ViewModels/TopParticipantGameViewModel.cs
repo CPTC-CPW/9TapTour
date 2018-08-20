ba13be1d63f545791f694b88c8c6732b3627e0fd
Aramirez1994 <Aramirez1994@live.com>
2018-08-20 13:07:59 -0700

    changed variable name

---
+++ NineTapTour/NineTapTour/Models/ViewModels/TopParticipantGameViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineTapTour.Models.ViewModels
+{
+    class TopParticipantGameViewModel
+    {
+        public TopParticipantGameViewModel(int memberID, string firstName, string lastName, int placing, int? scratchTotal, int? handicapScore, int? top3ScratchScore, int? top3HandiScores, int? game1, int? game2, int? game3, int? game4, int? handicap, int bonus, int gameID)
+        {
+            this.memberID = memberID;
+            FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+            LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+            Placing = placing;
+            ScratchTotal = scratchTotal ?? throw new ArgumentNullException(nameof(scratchTotal));
+
+            Top3ScratchScore = top3ScratchScore ?? throw new ArgumentNullException(nameof(top3ScratchScore));
+            Top3HandiScores = top3HandiScores ?? throw new ArgumentNullException(nameof(top3HandiScores));
+            Game1 = game1;
+            Game2 = game2;
+            Game3 = game3;
+            Game4 = game4;
+            Handicap = handicap ?? throw new ArgumentNullException(nameof(handicap));
+            Bonus = bonus;
+            GameID = gameID;
+            HandicapScore = this.ScratchTotal + (this.Handicap * 4) + (this.Bonus * 4); //TODO: implement flexibile num on ( 4 )
+        }
+
+//
+//        listOfTopScore[count].HandicapScore = totalScore + (listOfTopScore[count].Handicap * 4) + (listOfTopScore[count].Bonus* 4);//TODO: make "game count flexible"
+//        listOfTopScore[count].Top3ScratchScore = top3Games[0] + top3Games[1] + top3Games[2];
+//        listOfTopScore[count].Top3HandiScores = top3Games[0]
+
+        #region Properties
+
+        public int memberID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Placing { get; set; }
+        public int? ScratchTotal { get; set; }
+        public int? HandicapScore { get; set; }
+        public int? Top3ScratchScore { get; set; }
+        public int? Top3HandiScores { get; set; }
+        public int? Game1 { get; set; }
+        public int? Game2 { get; set; }
+        public int? Game3 { get; set; }
+        public int? Game4 { get; set; }
+        public int? Handicap { get; set; }
+        public int Bonus { get; set; }
+        public int GameID { get; set; }
+        #endregion
+    }
+}
