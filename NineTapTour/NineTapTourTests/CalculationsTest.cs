3f56160f78a47ae98dec00822e8a596ccdb76b09
Joe <joeprogrammer88@users.noreply.github.com>
2018-10-20 21:36:49 -0700

    Create unit test project. Start writing tests for calculate place standings

---
+++ NineTapTour/NineTapTourTests/CalculationsTest.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NineTapTour.Forms;
+using NineTapTour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineTapTour.Calculations.Test
+{
+    [TestClass()]
+    public class CalculationsTest
+    {
+        [TestMethod()]
+        public void CalculatePlaceStandings_RemovesDuplicateBowlers()
+        {
+            List<MemberScores> members = GetMemberScoreTestData();
+            int numBowlersWithDuplicates = members.Count;
+
+            Calculations.CalculatePlaceStandings(members);
+
+            int numBowlersAfterPlaceStandingsCalculation = members.Count;
+
+            Assert.AreNotEqual(numBowlersWithDuplicates, numBowlersAfterPlaceStandingsCalculation);
+        }
+
+        private static List<MemberScores> GetMemberScoreTestData()
+        {
+            return new List<MemberScores>()
+            {
+                new MemberScores()
+                {
+                    FirstName = "A", LastName = "A",  MemberId= 1, Score = 800
+                },
+                new MemberScores()
+                {
+                    FirstName = "B", LastName = "B",  MemberId= 2, Score = 800
+                },
+                new MemberScores()
+                {
+                    FirstName = "C", LastName = "C",  MemberId= 3, Score = 800
+                },
+                new MemberScores()
+                {
+                    FirstName = "D", LastName = "D",  MemberId= 4, Score = 800
+                },
+                new MemberScores()
+                {
+                    FirstName = "E", LastName = "E",  MemberId= 5, Score = 650
+                },
+                new MemberScores()
+                {
+                    FirstName = "F", LastName = "F",  MemberId= 6, Score = 650
+                },
+                new MemberScores()
+                {
+                    FirstName = "G", LastName = "G",  MemberId= 7, Score = 500
+                },
+                new MemberScores()
+                {
+                    FirstName = "A", LastName = "A",  MemberId= 1, Score = 1000
+                }
+            };
+        }
+
+        [TestMethod]
+        public void CalculatePlaceStandings_DisplaysTiesForBowlersWithSameScores()
+        {
+            List<MemberScores> members = GetMemberScoreTestData();
+            Calculations.CalculatePlaceStandings(members);
+            
+            //check first bowler is A with 1000
+            Assert.AreEqual(members[0].MemberId, 1);
+            Assert.AreEqual(members[0].Score, 1000);
+            Assert.AreEqual(members[0].placing, 1);
+
+            //check three way tie
+            Assert.AreEqual(members[1].placing, 2);
+            Assert.AreEqual(members[2].placing, 2);
+            Assert.AreEqual(members[3].placing, 2);
+        }
+    }
+}
