fa28a927daa7d000f7079703c8695470f9925152
Joe <joeprogrammer88@users.noreply.github.com>
2018-10-30 21:23:48 -0700

    Report Helper class was accidentally set to ignore

---
+++ NineTapTour/NineTapTour/Database/ReportHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineTapTour.Database
+{
+    public class ReportHelper
+    {
+        public enum ReportType
+        {   // 0 for High game handicap/senior, 1 for game/high game, 2 for series/high series
+            HighGameSenior = 0, HighGame = 1, HighSeries = 2
+        }
+    }
+}
