76c285ec64b6afbd5ba354f34125657bb61b0dfb
Anthony <anmccbusiness@gmail.com>
2018-11-13 12:12:07 -0800

    NO CODE CHANGES Updated comments for readability.

--- NineTapTour/NineTapTour/Migrations/Configuration.cs
+++ NineTapTour/NineTapTour/Migrations/Configuration.cs
@@ -21,39 +21,42 @@ namespace NineTapTour.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-
-        //This is a seed method using the bogus library for generating fake data https://github.com/bchavez/Bogus
+        // This is a seed method using the Bogus library for generating fake data: 
+        // https://github.com/bchavez/Bogus
         //
-        //This method will seed the database for Regions and Members as of the current database implementation 7/16/18
+        // This method will seed the database for Regions and Members as of the current 
+        // database implementation 7/16/18
         //
         #region Description
-        //Description Checks to see if a database currently exists
-        //If one does NOT exist build regions and then builds fake member data to replicate real world ninetap data that is configurable
-        //see instruction region below.
-
-
+        // Description Checks to see if a database currently exists
+        // If one does NOT exist build regions and then builds fake member data to 
+        // replicate real world ninetap data that is configurable
+        // see instruction region below.
         #endregion
 
         #region Instructions
-        //To rebuild/reconfigure the database go to the sql server object explorer then
-        //find the ninetap database right click and check close connection then hit ok
-        //and run UPDATE-DATABASE in the packamanger console
+        // To rebuild/reconfigure the database go to the sql server object explorer then
+        // find the ninetap database right click and check close connection then hit ok
+        // and run UPDATE-DATABASE in the packamanger console
         //
         //
-        //Use any of the read only fields to set and configure the data being created and follow the instructions of rebuild
-        //if you want to configure it further Look at the lambda expression chain in the Seed method and refer to https://github.com/bchavez/Bogus
-        //
-
-
+        // Use any of the read only fields to set and configure the data being created 
+        // and follow the instructions of rebuild if you want to configure it further.
+        // Look at the lambda expression chain in the Seed method and refer to:
+        // https://github.com/bchavez/Bogus
         #endregion
 
         #region Known Issues **READ THIS IF YOU ARE HAVING ISSUES**
-        //TODO: KNOWN ISSUES -> isLifeTime must be false until an error pertaining to the checking and unchecking of it is fixed.Refer to issue #304
-        //TODO:RegionId is set to only 1 region since an error occuring in sifting through members occurs when picking a region refer to issue #305 && #303
-        //TODO: 
+        // TODO: "FIXED IN ISSUE #311: https://github.com/CPTC-CPW/9TapTour/issues/304" 
+        //       -> KNOWN ISSUES -> isLifeTime must be false until an error pertaining to 
+        //       the checking and unchecking of it is fixed. Refer to issue #304 <- FIXED
+        //
+        // TODO: RegionId is set to only one region since an error occuring in sifting 
+        //       through members occurs when picking a region refer to issue #305 && #303:
+        //       https://github.com/CPTC-CPW/9TapTour/issues?q=is%3Aissue+305+is%3Aopen
         #endregion
 
-        //If you want to configure 
+        // If you want to configure 
         private readonly int _numOfMembersToGenerate = 200;
 
         private readonly int _startingRegionId = 1;
@@ -67,48 +70,50 @@ namespace NineTapTour.Migrations
 
         private readonly int _lowestBonusPin = 0;
         private readonly int _highestBonusPin = 5;
-        //this is used for the index global in bogus so we can start our members at a specific number and then increment by 1
-        //for example if we set this to 0 the first member created will start at 0 then the second will be 1 and so on.
+
+        // Variable _memberStartingNumber is used for the index global in bogus so we can 
+        // start our members at a specific number and then increment by 1
+        // For example if we set this to 0 the first member created will start at 0 then 
+        // the second will be 1 and so on.
         private readonly int _memberStartingNumber = 1;
 
         private readonly int _scoreAdjuster = 15;
 
-        //THIS SHOULD ALWAYS BE A NEGATIVE NUMBER.
-        //This will set the earliest year possible (in comparison to the date when running update-database )when creating fake members and their joined dates.
+        // Variable _earliestJoinDate SHOULD ALWAYS BE A NEGATIVE NUMBER.
+        // This will set the earliest year possible (in comparison to the date when running 
+        // Update-Database) when creating fake members and their joined dates.
+        private readonly DateTime _earliestJoinDate = DateTime.Now.AddYears(-10); // <-- Negative Num Required
 
-        private readonly DateTime _earliestJoinDate = DateTime.Now.AddYears(-10);//This should always be a negative number
         private readonly DateTime _latestJoinDate = DateTime.Now;
-
         private readonly int _maxSquads = 4;
 
-        //Review the documentation before changing anything directly in the method
+        // Review the documentation before changing anything directly in the Seed method
 #if DEBUG
-
-
         protected override void Seed(NineTapTour.Database.NineTapDb context)
         {
-
             if (!context.Members.Any())
             {
-                //incrementer local variables to prevent data mismatch in areas of the database that are not related correctly.
-                //**DO NOT TOUCH THESE UNLESS THERE IS A REALL GOOD REASON LIKE A DATABASE CHANGE.**
+                // Incrementer local variables to prevent data mismatch in areas of the database 
+                // that are not related correctly.
+                // **DO NOT TOUCH THESE UNLESS THERE IS A REAL GOOD REASON, LIKE A DATABASE CHANGE.**
 
-                #region Info about these **DO NOT TOUCH THESE UNLESS THERE IS A REALL GOOD REASON LIKE A DATABASE CHANGE.**
-                //Since tinkering with bogus, which is meant for relational databases and the current state of the database does not use
-                //this as it should there are some work arounds that I had to implement to avoid data mismatch
-                //index is use to keep track of what index we are currently at in the list to replicate the exact same data in other tables
+                #region Info about these **DO NOT TOUCH THESE UNLESS THERE IS A REAL GOOD REASON, LIKE A DATABASE CHANGE.**
+                // Since tinkering with Bogus, which is meant for relational databases and the 
+                // current state of the database does not use this as it should there are some 
+                // work arounds that I had to implement to avoid data mismatch. Index is use to 
+                // keep track of what index we are currently at in the list to replicate the exact 
+                // same data in other tables.
                 //
-                //The two currently are handicap and bonus -> Basically when a member is created their handicap and bonus are added to a list
-                //then when a game is created the handi and bonus list, are tapped into and take the paralleled index for values.
-
-
+                // The two indexes currently are handicap and bonus -> Basically when a member is 
+                // created their handicap and bonus are added to a list then when a game is created 
+                // the handicap and bonus list are tapped into and take the parallel index for values.
                 #endregion
 
                 int index = 0;
                 List<int> handicapList = new List<int>();
                 List<int> bonusList = new List<int>();
-                
-                //Create Regions
+
+                // Create Regions
                 if (!context.NineTapRegion.Any())
                 {
                     context.NineTapRegion.AddOrUpdate(r => r.NineTapRegionID,
@@ -117,30 +122,33 @@ namespace NineTapTour.Migrations
                     );
                 }
 
-                //Create Tournament, this only generates 1 change generation to allow for more (you will have to change participants below
-                //and generate them into random tournaments if you want them to be spread out.
+                // Create Tournament, this only generates one change generation to allow for more (you 
+                // will have to change participants below and generate them into random tournaments if 
+                // you want them to be spread out).
+
                 #region Additional info about tournament 
-                //We only create one tournament as that's all we need for basic testing for most things.
-                //this can be expanded upon but you will need to correctly think about how the calculations would progress
-                //with each new tournament, an alternative is to create more and then go through each tournament and finalize the scores.
-                //if you really wanted to test entries over time calculation.
-
-
+                // We only create one tournament as that's all we need for basic testing for most things.
+                // This can be expanded upon but you will need to correctly think about how the calculations 
+                // would progress with each new tournament, an alternative is to create more and then go 
+                // through each tournament and finalize the scores. If you really wanted to test entries over 
+                // time calculation.
                 #endregion
-                var tournamentSeed = new Bogus.Faker<Tournament>().Rules((f, t) =>
-                {
-                    t.Date = DateTime.Now;
-                    t.Location = f.Address.City();
-                    t.Event = $"SomeTournament {index}";
-                    t.Notes = f.Lorem.Sentence();
-                    t.Sponsors = f.Company.CompanyName();
-                    t.Squads = _maxSquads;
-                    t.Doubles = false;
-                    t.ThreeOutOf4 = false;
-                    t.TourneyRegion = 1;
-                }).Generate(1);
                 
-                //Creates members and seeds in all important information, and some extra information to simulate.
+                var tournamentSeed = new Bogus.Faker<Tournament>().Rules((f, t) =>
+                    {
+                        t.Date = DateTime.Now;
+                        t.Location = f.Address.City();
+                        t.Event = $"SomeTournament {index}";
+                        t.Notes = f.Lorem.Sentence();
+                        t.Sponsors = f.Company.CompanyName();
+                        t.Squads = _maxSquads;
+                        t.Doubles = false;
+                        t.ThreeOutOf4 = false;
+                        t.TourneyRegion = 1;
+                    }).Generate(1);
+
+                // Creates members and seeds in all important information, and some extra information 
+                // to simulate.
                 var memberSeed = new Bogus.Faker<Member>()
                     .Rules((f, m) =>
                     {
@@ -172,9 +180,12 @@ namespace NineTapTour.Migrations
 
                 var gameSeed = new Bogus.Faker<Game>().Rules((f, g) => {
                     g.Game1 = f.Random.Number(100, 280);
-                    g.Game2 = g.Game1 - f.Random.Number(-_scoreAdjuster, _scoreAdjuster);//These values off set the scores they bowled
+
+                    // These values off set the scores they bowled
+                    g.Game2 = g.Game1 - f.Random.Number(-_scoreAdjuster, _scoreAdjuster);
                     g.Game3 = g.Game1 - f.Random.Number(-_scoreAdjuster, _scoreAdjuster);
                     g.Game4 = g.Game1 - f.Random.Number(-_scoreAdjuster, _scoreAdjuster);
+
                     g.Notes = null;
                     g.Handicap = handicapList[index];
                     g.TotalScore = g.Game1 + g.Game2 + g.Game3 + g.Game4;
@@ -183,7 +194,8 @@ namespace NineTapTour.Migrations
                     g.InputtedAvg = g.TotalScore / 4;
                     g.MoneyWon = f.Random.Decimal(0, 0);
                 });
-
+                
+                // Original
                 var participantSeed = new Bogus.Faker<Participant>().Rules((f, p) =>
                 {
                     p.Member = memberSeed;
@@ -193,8 +205,9 @@ namespace NineTapTour.Migrations
                     p.Tournament = tournamentSeed[0];
                 })
                 .Generate(_numOfMembersToGenerate);
-                
-                //At this point you will generate one member per participant, that will also have a game related to a single tournament.
+
+                // At this point you will generate one member per participant, that will also have 
+                // a game related to a single tournament.
                 context.Participants.AddRange(participantSeed);
             }
             context.SaveChanges();
