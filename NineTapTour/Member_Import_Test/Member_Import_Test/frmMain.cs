279d5d3bb488cf3ff0e75d527ccb83041d777e73
Robert Wood Jr <rdrwood@gmail.com>
2018-11-29 11:29:03 -0800

    cleaned up { formating

--- NineTapTour/Member_Import_Test/Member_Import_Test/frmMain.cs
+++ NineTapTour/Member_Import_Test/Member_Import_Test/frmMain.cs
@@ -756,7 +756,7 @@ namespace Member_Import_Test
                     playerH.regionID = RegionID;
                     //only process file if they have been added as a member first and are active ?
                         if (MemberDb.GetMember(temp.PlayerNumber, RegionID).IsActive == true)
-                    {
+                        {
                             try
                             {
                                 temp.GameTotal = Convert.ToInt32((range.Cells[row, 1] as Excel.Range).Value2);
