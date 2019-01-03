ab5742473e9a99e1871e29d876ea0f7fc34b6252
Robert Wood Jr <rdrwood@gmail.com>
2019-01-02 23:29:01 -0800

    code clean up for money import to player history

--- NineTapTour/NineTapTour/Forms/FrmMemberData.cs
+++ NineTapTour/NineTapTour/Forms/FrmMemberData.cs
@@ -1246,18 +1246,25 @@ namespace NineTapTour.Forms
 
                 for (int row = rowNum; row <= range.Rows.Count; row++)
                 {
- 
+
                     ExcelRow temp = new ExcelRow();
                     PlayerHistory playerH = new PlayerHistory();
                     Game GameHistory = new Game();
 
+                    string game1 = Convert.ToString((range.Cells[row, 3] as Excel.Range).Value2);
+                    string game2 = Convert.ToString((range.Cells[row, 3] as Excel.Range).Value2);
+                    string game3 = Convert.ToString((range.Cells[row, 3] as Excel.Range).Value2);
+                    string game4 = Convert.ToString((range.Cells[row, 3] as Excel.Range).Value2);
+                    string testpot = Convert.ToString((range.Cells[row, 6] as Excel.Range).Value2);
+
                     if (row != subRow)
                     {
-                        if (Convert.ToInt32((range.Cells[row, 3] as Excel.Range).Value2) == 0 &&
-                        Convert.ToInt32((range.Cells[row, 4] as Excel.Range).Value2) == 0 &&
-                        Convert.ToInt32((range.Cells[row, 5] as Excel.Range).Value2) == 0 &&
-                        Convert.ToInt32((range.Cells[row, 6] as Excel.Range).Value2) == 0 &&
-                        Convert.ToString((range.Cells[row, 14] as Excel.Range).Value2) != null
+                        if(
+                            string.IsNullOrWhiteSpace(game1) &&
+                            string.IsNullOrWhiteSpace(game2) &&
+                            string.IsNullOrWhiteSpace(game3) &&
+                            string.IsNullOrWhiteSpace(game4) &&
+                            !string.IsNullOrWhiteSpace(testpot)
                         )
                         {
                             noGameMoneyWon += Convert.ToDouble((range.Cells[row, 15] as Excel.Range).Value2);
@@ -1265,9 +1272,23 @@ namespace NineTapTour.Forms
                         }
                     }
 
+                    //if (row != subRow)
+                    //{
+                    //    if (Convert.ToInt32((range.Cells[row, 3] as Excel.Range).Value2) == 0 &&
+                    //    Convert.ToInt32((range.Cells[row, 4] as Excel.Range).Value2) == 0 &&
+                    //    Convert.ToInt32((range.Cells[row, 5] as Excel.Range).Value2) == 0 &&
+                    //    Convert.ToInt32((range.Cells[row, 6] as Excel.Range).Value2) == 0 &&
+                    //    Convert.ToString((range.Cells[row, 14] as Excel.Range).Value2) != null
+                    //    )
+                    //    {
+                    //        noGameMoneyWon += Convert.ToDouble((range.Cells[row, 15] as Excel.Range).Value2);
+                    //        continue;
+                    //    }
+                    //}
 
 
-            GameHistory.gameRegionID = RegionID;
+
+                    GameHistory.gameRegionID = RegionID;
                     temp.PlayerFirstName = PlayerFinalFirstAndMiddle[0];
                     temp.PlayerMiddleName = PlayerFinalFirstAndMiddle[1];
                     temp.PlayerLastName = playerLastName;
