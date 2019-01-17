bacf24e77b29ed8230ee9381e4fb111a5c8b26b0
Robert Wood Jr <rdrwood@gmail.com>
2019-01-16 16:08:50 -0800

    added file for the class

---
+++ NineTapTour/NineTapTour/Models/TempVariablesForGlobalLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineTapTour.Models
+{
+    static class TempVariablesForGlobalLevel
+    {
+        static List<double> MoneyEarnings { get; set; }
+
+    }
+}
