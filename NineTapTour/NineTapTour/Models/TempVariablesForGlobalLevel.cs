b34534a8363ae79ea9b6d64df2c2617b5fdf1940
Robert Wood Jr <rdrwood@gmail.com>
2019-01-17 13:06:03 -0800

    added some documentation notes about the purpose of the class TempVariablesForGlobalLevel

--- NineTapTour/NineTapTour/Models/TempVariablesForGlobalLevel.cs
+++ NineTapTour/NineTapTour/Models/TempVariablesForGlobalLevel.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace NineTapTour.Models
 {
+     /// <summary>
+     /// This class is made to hold and store variables for use in a form 
+     /// and held when the form is closed allowing them to be useable
+     /// again when the form is reopened. For example if you
+     /// wanted to access store a game score in form 1 but needed to go look 
+     /// at something in form 2, when you reopened form 1 it would be 
+     /// accesible in form 1 again.
+     /// </summary>
     public static class TempVariablesForGlobalLevel
     {
+    
+        
         public static List<double> MoneyEarnings { get; set; }
 
     }
