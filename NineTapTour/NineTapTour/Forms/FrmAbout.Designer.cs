30637446da15f4a440cd39b679bfd9e4d9843d3d
Amy Dixon <amysha.dixon@gmail.com>
2019-01-30 15:16:28 -0800

    Fixed double AboutToolStripMenuItem in FrmMain.Designer.cs that occured during a mysterious merge -- was causing an error when FrmMain.cs was opened.

--- NineTapTour/NineTapTour/Forms/FrmAbout.Designer.cs
+++ NineTapTour/NineTapTour/Forms/FrmAbout.Designer.cs
@@ -37,7 +37,7 @@
             this.rtbFrmAboutText.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.rtbFrmAboutText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rtbFrmAboutText.ForeColor = System.Drawing.Color.White;
-            this.rtbFrmAboutText.Location = new System.Drawing.Point(98, 188);
+            this.rtbFrmAboutText.Location = new System.Drawing.Point(96, 225);
             this.rtbFrmAboutText.Name = "rtbFrmAboutText";
             this.rtbFrmAboutText.ReadOnly = true;
             this.rtbFrmAboutText.Size = new System.Drawing.Size(662, 240);
