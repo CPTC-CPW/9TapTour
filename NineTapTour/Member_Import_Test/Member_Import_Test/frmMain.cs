ca40cefe45eccff8faf40261fbc765642628bfd7
Rob Wood <rdrwood@gmail.com>
2019-03-08 10:43:04 -0800

    import change

--- NineTapTour/Member_Import_Test/Member_Import_Test/frmMain.cs
+++ NineTapTour/Member_Import_Test/Member_Import_Test/frmMain.cs
@@ -695,6 +695,10 @@ namespace Member_Import_Test
                     string game3 = Convert.ToString((range.Cells[row, 5] as Excel.Range).Value2);
                     string game4 = Convert.ToString((range.Cells[row, 6] as Excel.Range).Value2);
                     string testFin = Convert.ToString((range.Cells[row, 14] as Excel.Range).Value2);
+                    if (Convert.ToString((range.Cells[row, 1] as Excel.Range).Value2) == 0)
+                    {
+                        continue;
+                    }
 
                     if ( // if no date or cash then continue to the next line
                         string.IsNullOrWhiteSpace(Convert.ToString((range.Cells[row, 2] as Excel.Range).Value2)) &&
