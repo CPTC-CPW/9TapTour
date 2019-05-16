c6c9d7994c009d512e0c5910d4dfc6d2429ea62b
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2019-05-15 20:23:04 -0700

    Simplify getFinalizeID method. Remove unnecessary loop

--- NineTapTour/NineTapTour/Database/FinalizeTempDB.cs
+++ NineTapTour/NineTapTour/Database/FinalizeTempDB.cs
@@ -154,67 +154,14 @@ namespace NineTapTour.Database
 
         public static FinalizeTemp getFinalizeID(Game currentG)
         {
-            FinalizeTemp ft = new FinalizeTemp();
             var db = new NineTapDb();
-            var temp = (
-
-                from par in db.FinalizeTemp
+            var finalizeTemp = 
+                (from par in db.FinalizeTemp
                 where par.GameId == currentG.Id
-                select new
-                {
-
-                    par.AdjustedAvg,
-                    par.Bonus,
-                    par.FinalizeID,
-                    par.FirstName,
-                    par.Game1,
-                    par.Game2,
-                    par.Game3,
-                    par.Game4,
-                    par.GameAvg,
-                    par.GameId,
-                    par.Handicap,
-                    par.KeepAdjustedAvg,
-                    par.LastName,
-                    par.LeagueAverage,
-                    par.MemberId,
-                    par.Notes,
-                    par.ScratchTotal,
-                    par.Squad,
-                    par.TournamentID,
-                    par.UseGame1,
-                    par.UseGame2,
-                    par.UseGame3,
-                    par.UseGame4
-                });
-            foreach (var i in temp)
-            {
-                ft.AdjustedAvg = i.AdjustedAvg;
-                ft.Bonus = i.Bonus;
-                ft.FinalizeID = i.FinalizeID;
-                ft.FirstName = i.FirstName;
-                ft.Game1 = i.Game1;
-                ft.Game2 = i.Game2;
-                ft.Game3 = i.Game3;
-                ft.Game4 = i.Game4;
-                ft.GameAvg = i.GameAvg;
-                ft.GameId = i.GameId;
-                ft.Handicap = i.Handicap;
-                ft.KeepAdjustedAvg = i.KeepAdjustedAvg;
-                ft.LastName = i.LastName;
-                ft.LeagueAverage = i.LeagueAverage;
-                ft.MemberId = i.MemberId;
-                ft.Notes = i.Notes;
-                ft.ScratchTotal = i.ScratchTotal;
-                ft.Squad = i.Squad;
-                ft.TournamentID = i.TournamentID;
-                ft.UseGame1 = i.UseGame1;
-                ft.UseGame2 = i.UseGame2;
-                ft.UseGame3 = i.UseGame3;
-                ft.UseGame4 = i.UseGame4;
-               
-            }
-            return ft;
+                select par)
+                    .SingleOrDefault();
+            //return empty finalize object if none are found to retain original behavior
+            return finalizeTemp is null ? new FinalizeTemp() : finalizeTemp;
         }
 
         /// <summary>
