33f4c0cb4b23c9e80be3020e5d1c030e935e675e
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2019-05-16 22:10:46 -0700

    Remove unneeded sort by standing and sort by name

--- NineTapTour/NineTapTour/Forms/FrmFinalizeTournament.cs
+++ NineTapTour/NineTapTour/Forms/FrmFinalizeTournament.cs
@@ -380,8 +380,6 @@ namespace NineTapTour.Forms
             TournamentEntriesGrid.Columns[GAME_3_VALID_COLUMN].HeaderText = string.Empty;
             TournamentEntriesGrid.Columns[GAME_4_VALID_COLUMN].HeaderText = string.Empty;
 
-            //Sort DataGridView by Standing
-            SortByStanding();
 #if DEBUG
             // resets the adjusted averages
             ResetAdjustedAverages(FinalizeTableList);
@@ -688,38 +686,6 @@ namespace NineTapTour.Forms
                     Rank++;
                 }
             }
-        }
-
-        /// <summary>
-        /// This method is for the radio buttons that sorting the TournamentEntriesGrid by either scores or names
-        /// </summary>
-        private void rbnStanding_CheckedChanged(object sender, EventArgs e)
-        {
-            if (rbnStanding.Checked == true)
-            {
-                SortByStanding();
-            }
-            // if rbnName.Checked == true
-            else
-            {
-                SortByName();
-            }
-        }
-
-        /// <summary>
-        /// Sorts TournamentEntriesGrid by the scores of the entries, in descending order
-        /// </summary>
-        public void SortByStanding()
-        {
-            TournamentEntriesGrid.Sort(TournamentEntriesGrid.Columns[0], ListSortDirection.Descending);
-        }
-
-        /// <summary>
-        /// Sorts TournamentEntriesGrid by the names of the entries, in ascending order
-        /// </summary>
-        private void SortByName()
-        {
-            TournamentEntriesGrid.Sort(TournamentEntriesGrid.Columns[2], ListSortDirection.Ascending);
         }
 
         /// <summary>
