4c92b3e18fdabc38afaf195b804dbf574750db50
Tina Greene <greene9408@student.cptc.edu>
2019-07-15 14:46:53 -0700

    Deleted LeagueAverage() function from MemberData.cs. This function is not being called. The average is being calculated using PlayerHistoryDB starting on line 554

--- NineTapTour/NineTapTour/Forms/FrmMemberData.cs
+++ NineTapTour/NineTapTour/Forms/FrmMemberData.cs
@@ -908,40 +908,7 @@ namespace NineTapTour.Forms
         }
 
      
-        public double LeagueAverage (Member mem)
-        {
-            double sum = 0;
-            double average = 0;
-            var db = new NineTapDb();
-
-            var temp = (
-                        from p in db.Participants
-                        join m in db.Members on p.Member.Id equals m.Id
-                        join g in db.Games on p.Game.Id equals g.Id
-                        join t in db.Tournaments on p.Tournament.Id equals t.Id
-                        where mem.Id == m.Id
-                        orderby t.Date descending
-                        select new
-                        {
-                            t.Date,
-                            g.Game1,
-                            g.Game2,
-                            g.Game3,
-                            g.Game4,
-                            Average = (g.Game1 + g.Game2 + g.Game3 + g.Game4) / 4
-                        }).Take(30).ToList();
-
-            if(temp.Count > 0)
-            {
-                foreach (var item in temp)
-                {
-                    sum += Convert.ToDouble(item.Average);
-                }
-                return (average = sum / temp.Count());
-            }
-            return 0 ;
-        }
-
+  
         public double LeagueAvgFromPlayerHistory(Member mem)
         {
             double sum = 0;
