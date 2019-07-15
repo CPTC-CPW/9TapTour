3abe073ca1d39858fbd29884945707102c2f4cfb
Zachary Bitseff <zachbitseff@gmail.com>
2019-07-15 11:51:56 -0700

    Remove rtbFrmAboutText and replace with lblFrmAboutText.

--- NineTapTour/NineTapTour/Forms/FrmAbout.cs
+++ NineTapTour/NineTapTour/Forms/FrmAbout.cs
@@ -14,20 +14,6 @@ namespace NineTapTour.Forms {
     public partial class FrmAbout : Form {
         public FrmAbout() {
             InitializeComponent();
-
-            rtbFrmAboutText.Enabled = true; 
-            rtbFrmAboutText.TabStop = false;
-
-            /* Fills the rtbFrmAbout text box with the information below */
-            rtbFrmAboutText.Text = "9 -Tap Tour Inc. is a unique, fun, and professionally run tournament. " +
-                "Members enjoy our \"Beat the Board\" format, with four games per squad. The fun, big payouts, " +
-                "special pots, and the 9-Tap version of bowling itself, brings new excitement to tournaments. \n \n " +
-                "Approximately one bowler in every 5 entries will cash out. Other optional ways to cash are: 9-Tap " +
-                "Jackpots, Progressive Pots, High Game Pots, Brackets, Scratch Games, Series Pots, and more; " +
-                "Depending on where and when you bowl, these Side Pots may vary from time to time. 9-Tap Tour " +
-                "also has BIG added tournaments. Each quarterly tournament may have eligibility requirements for " +
-                "members who bowl during that quarter. \n";
-            rtbFrmAboutText.ReadOnly = true;
         }
 
     }
