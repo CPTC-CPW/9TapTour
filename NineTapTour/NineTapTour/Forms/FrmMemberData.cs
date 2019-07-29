1bee25428651f305ab5bc78020e5224f1550233e
Zach Bitseff <38227293+zachbitseff@users.noreply.github.com>
2019-07-29 09:34:50 -0700

    Revert "Member Info Save multi messages"

--- NineTapTour/NineTapTour/Forms/FrmMemberData.cs
+++ NineTapTour/NineTapTour/Forms/FrmMemberData.cs
@@ -632,7 +632,9 @@ namespace NineTapTour.Forms
                 try
                 {
                     MemberDB.AddOrUpdateMember(temp);
-
+#if DEBUG
+                    MessageBox.Show("Member saved");
+#endif
                     ((FrmMain)MdiParent)._membersList = 
                         MemberDB.GetMemberList(RegionID).OrderBy(m => m.Number);
                     UpdateMemberInfo();
