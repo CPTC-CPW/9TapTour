c7011d669ce1c078e30c2515128626e01fa8dfb7
Steven Ballard <stevenballardstudios@gmail.com>
2019-10-24 10:52:10 -0700

    Moved the button according to Ken's input

--- NineTapTour/NineTapTour/Forms/FrmFinalizeTournament.Designer.cs
+++ NineTapTour/NineTapTour/Forms/FrmFinalizeTournament.Designer.cs
@@ -89,7 +89,7 @@
             this.btnFinalize.FlatAppearance.BorderSize = 2;
             this.btnFinalize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnFinalize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFinalize.Location = new System.Drawing.Point(221, 1);
+            this.btnFinalize.Location = new System.Drawing.Point(1108, 1);
             this.btnFinalize.Name = "btnFinalize";
             this.btnFinalize.Size = new System.Drawing.Size(100, 28);
             this.btnFinalize.TabIndex = 1;
