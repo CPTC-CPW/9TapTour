b3a52918ad36ec749314242f2b206c446c31e016
lampert309 <lampert309@gmail.com>
2020-01-28 14:06:58 -0800

    Changed all references of the file to FrmTournamentsByYear to match the rename

---
+++ NineTapTour/NineTapTour/Forms/FrmTournamentsByYear.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace NineTapTour.Forms
+{
+    partial class FrmTournamentsByYear
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmTournamentsByYear));
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvAllTournaments = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxYear = new System.Windows.Forms.ComboBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAllTournaments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tournaments by Year";
+            // 
+            // dgvAllTournaments
+            // 
+            this.dgvAllTournaments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAllTournaments.Location = new System.Drawing.Point(10, 118);
+            this.dgvAllTournaments.Margin = new System.Windows.Forms.Padding(1);
+            this.dgvAllTournaments.Name = "dgvAllTournaments";
+            this.dgvAllTournaments.RowTemplate.Height = 37;
+            this.dgvAllTournaments.Size = new System.Drawing.Size(863, 414);
+            this.dgvAllTournaments.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(360, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Search by Year";
+            // 
+            // cbxYear
+            // 
+            this.cbxYear.FormattingEnabled = true;
+            this.cbxYear.Location = new System.Drawing.Point(359, 31);
+            this.cbxYear.Margin = new System.Windows.Forms.Padding(1);
+            this.cbxYear.Name = "cbxYear";
+            this.cbxYear.Size = new System.Drawing.Size(82, 21);
+            this.cbxYear.TabIndex = 3;
+            this.cbxYear.SelectedIndexChanged += new System.EventHandler(this.cbxYear_SelectedIndexChanged);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(361, 55);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(1);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(74, 22);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // TournamentsByYear
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 540);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.cbxYear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvAllTournaments);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "TournamentsByYear";
+            this.Text = "List of Tournaments By Year";
+            this.Load += new System.EventHandler(this.TournamentsByYear_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAllTournaments)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvAllTournaments;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxYear;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
