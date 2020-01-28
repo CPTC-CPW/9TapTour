f81052caa17f0ce0e56b5fff684d23979f1c7025
lampert309 <lampert309@gmail.com>
2020-01-28 14:14:44 -0800

    Renamed the file 'PlayerHistoryForm' to 'FrmPlayerHistory' to match the naming convention used

---
+++ NineTapTour/NineTapTour/Forms/FrmPlayerHistory.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace NineTapTour.Database
+{
+    partial class PlayerHistoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayerHistoryForm));
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.lblMemberNumber = new System.Windows.Forms.Label();
+            this.lblMemberSrartAvg = new System.Windows.Forms.Label();
+            this.dtvPlayerHistory = new System.Windows.Forms.DataGridView();
+            this._NineTapTour_NineTapDbDataSet = new NineTapTour._NineTapTour_NineTapDbDataSet();
+            this.tournamentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tournamentsTableAdapter = new NineTapTour._NineTapTour_NineTapDbDataSetTableAdapters.TournamentsTableAdapter();
+            this.fKdboParticipantsdboTournamentsTournamentIdBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nineTapTourNineTapDbDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dtvPlayerHistory)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._NineTapTour_NineTapDbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tournamentsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fKdboParticipantsdboTournamentsTournamentIdBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nineTapTourNineTapDbDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Font = new System.Drawing.Font("Calibri", 30F, System.Drawing.FontStyle.Bold);
+            this.lblFullName.Location = new System.Drawing.Point(22, 9);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(141, 49);
+            this.lblFullName.TabIndex = 3;
+            this.lblFullName.Text = "#Name";
+            // 
+            // lblMemberNumber
+            // 
+            this.lblMemberNumber.AutoSize = true;
+            this.lblMemberNumber.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Bold);
+            this.lblMemberNumber.Location = new System.Drawing.Point(25, 68);
+            this.lblMemberNumber.Name = "lblMemberNumber";
+            this.lblMemberNumber.Size = new System.Drawing.Size(205, 33);
+            this.lblMemberNumber.TabIndex = 4;
+            this.lblMemberNumber.Text = "MemberNumber";
+            // 
+            // lblMemberSrartAvg
+            // 
+            this.lblMemberSrartAvg.AutoSize = true;
+            this.lblMemberSrartAvg.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Bold);
+            this.lblMemberSrartAvg.Location = new System.Drawing.Point(25, 111);
+            this.lblMemberSrartAvg.Name = "lblMemberSrartAvg";
+            this.lblMemberSrartAvg.Size = new System.Drawing.Size(206, 33);
+            this.lblMemberSrartAvg.TabIndex = 5;
+            this.lblMemberSrartAvg.Text = "MemberStartavg";
+            // 
+            // dtvPlayerHistory
+            // 
+            this.dtvPlayerHistory.AllowUserToDeleteRows = false;
+            this.dtvPlayerHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtvPlayerHistory.Location = new System.Drawing.Point(31, 164);
+            this.dtvPlayerHistory.Name = "dtvPlayerHistory";
+            this.dtvPlayerHistory.ReadOnly = true;
+            this.dtvPlayerHistory.Size = new System.Drawing.Size(643, 243);
+            this.dtvPlayerHistory.TabIndex = 6;
+            // 
+            // _NineTapTour_NineTapDbDataSet
+            // 
+            this._NineTapTour_NineTapDbDataSet.DataSetName = "_NineTapTour_NineTapDbDataSet";
+            this._NineTapTour_NineTapDbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tournamentsBindingSource
+            // 
+            this.tournamentsBindingSource.DataMember = "Tournaments";
+            this.tournamentsBindingSource.DataSource = this._NineTapTour_NineTapDbDataSet;
+            // 
+            // tournamentsTableAdapter
+            // 
+            this.tournamentsTableAdapter.ClearBeforeFill = true;
+            // 
+            // fKdboParticipantsdboTournamentsTournamentIdBindingSource
+            // 
+            this.fKdboParticipantsdboTournamentsTournamentIdBindingSource.DataMember = "FK_dbo.Participants_dbo.Tournaments_Tournament_Id";
+            this.fKdboParticipantsdboTournamentsTournamentIdBindingSource.DataSource = this.tournamentsBindingSource;
+            // 
+            // nineTapTourNineTapDbDataSetBindingSource
+            // 
+            this.nineTapTourNineTapDbDataSetBindingSource.DataSource = this._NineTapTour_NineTapDbDataSet;
+            this.nineTapTourNineTapDbDataSetBindingSource.Position = 0;
+            // 
+            // PlayerHistoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(709, 419);
+            this.Controls.Add(this.dtvPlayerHistory);
+            this.Controls.Add(this.lblMemberSrartAvg);
+            this.Controls.Add(this.lblMemberNumber);
+            this.Controls.Add(this.lblFullName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "PlayerHistoryForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Player History";
+            this.Load += new System.EventHandler(this.PlayerHistoryForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtvPlayerHistory)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._NineTapTour_NineTapDbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tournamentsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fKdboParticipantsdboTournamentsTournamentIdBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nineTapTourNineTapDbDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.Label lblMemberNumber;
+        private System.Windows.Forms.Label lblMemberSrartAvg;
+        private System.Windows.Forms.DataGridView dtvPlayerHistory;
+        private _NineTapTour_NineTapDbDataSet _NineTapTour_NineTapDbDataSet;
+        private System.Windows.Forms.BindingSource tournamentsBindingSource;
+        private _NineTapTour_NineTapDbDataSetTableAdapters.TournamentsTableAdapter tournamentsTableAdapter;
+        private System.Windows.Forms.BindingSource fKdboParticipantsdboTournamentsTournamentIdBindingSource;
+        private System.Windows.Forms.BindingSource nineTapTourNineTapDbDataSetBindingSource;
+    }
+}
---
+++ NineTapTour/NineTapTour/Forms/FrmPlayerHistory.cs
@@ -0,0 +1,108 @@
+﻿using NineTapTour.Database;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Drawing.Printing;
+using System.Configuration;
+using System.Data.Entity.Core.Objects;
+using NineTapTour.Models;
+
+namespace NineTapTour.Database
+{
+    public partial class PlayerHistoryForm : Form
+    {
+        private int id;
+       
+        public PlayerHistoryForm(int id)
+        {
+            InitializeComponent();
+            this.id = id;
+            
+            Member currentMember = MemberDB.GetMember(id,0 );
+
+            createDataGridView();
+
+            lblFullName.Text = ($"Name : {currentMember.FirstName} {currentMember.LastName}");
+            lblMemberNumber.Text = ($"Member Number: {currentMember.Number}");
+            lblMemberSrartAvg.Text = ($"Start avg : {currentMember.StartAvg}");
+        }
+
+        private void PlayerHistoryForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the '_NineTapTour_NineTapDbDataSet.Tournaments' table. You can move, or remove it, as needed.
+            this.tournamentsTableAdapter.Fill(this._NineTapTour_NineTapDbDataSet.Tournaments);
+        }
+
+
+        private void createDataGridView()
+        {
+            List<PlayerHistory> playerHistory = PlayerHistoryDB.GetTop30FromPlayerHistory(id);
+            dtvPlayerHistory.DataSource = dataView(playerHistory);
+
+            dtvPlayerHistory.SuspendLayout();
+            var column = dtvPlayerHistory.Columns[1];
+            for(int i = 2; i <= 15;  i++)
+            {
+                column = dtvPlayerHistory.Columns[i];
+                column.Width = 50;
+            }
+            dtvPlayerHistory.ResumeLayout();
+            dtvPlayerHistory.AllowUserToAddRows = false;
+        }
+
+
+        private DataTable dataView(List<PlayerHistory> playerHistory)
+        {
+            var db = new NineTapDb();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Games Played").ReadOnly = true;
+            dt.Columns.Add("Date").ReadOnly = true;
+            dt.Columns.Add("Game 1").ReadOnly = true;
+            dt.Columns.Add("Game 2").ReadOnly = true;
+            dt.Columns.Add("Game 3").ReadOnly = true;
+            dt.Columns.Add("Game 4").ReadOnly = true;
+            dt.Columns.Add("Total").ReadOnly = true;
+            dt.Columns.Add("Average of Row").ReadOnly = true;
+            dt.Columns.Add("True Average").ReadOnly = true;
+            dt.Columns.Add("AVG").ReadOnly = true;
+            dt.Columns.Add("HandiCap").ReadOnly = true;
+            dt.Columns.Add("Bonus").ReadOnly = true;
+            dt.Columns.Add("ProPot").ReadOnly = true;
+            dt.Columns.Add("PPHG").ReadOnly = true;
+            dt.Columns.Add("Cash").ReadOnly = true;
+            dt.Columns.Add("Notes").ReadOnly = true;
+
+            List<PlayerHistory> temp = playerHistory;
+
+            foreach (var item in temp)
+            {
+                DataRow newRow = dt.NewRow();
+                newRow["Games Played"] = item.GamesPlayed;
+                newRow["Date"] = item.TournamentDate.ToShortDateString();
+                newRow["Game 1"] = item.Game1;
+                newRow["Game 2"] = item.Game2;
+                newRow["Game 3"] = item.Game3;
+                newRow["Game 4"] = item.Game4;
+                newRow["Total"] = item.TotalScore;
+                newRow["Average of Row"] = item.AverageForEntry;
+                newRow["True Average"] = item.trueAVG;
+                newRow["AVG"] = item.AVG;
+                newRow["HandiCap"] = item.HandiCap;
+                newRow["Bonus"] = item.Bonus;
+                newRow["ProPot"] = item.ProPot;
+                newRow["PPHG"] = item.PPHG;
+                newRow["Cash"] = item.MoneyWon;
+                newRow["Notes"] = item.Notes;
+
+                dt.Rows.Add(newRow);
+            }
+            return dt;
+        }
+    }
+}
