a4c1a7ee2014f2c3065ee190484e60961ba362b2
lampert309 <lampert309@gmail.com>
2020-01-29 14:08:26 -0800

    Moved the code that gets a tournamentStatsList into a TournamentStatsListDB method

--- NineTapTour/NineTapTour/Database/TournamentStatsListDB.cs
+++ NineTapTour/NineTapTour/Database/TournamentStatsListDB.cs
@@ -3,11 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NineTapTour.Models;
 
 namespace NineTapTour.Database
 {
     class TournamentStatsListDB
     {
+        public static List<TournamentStatsList> GetTournamentStatsList(int selectedTournament)
+        {
+            using (NineTapDb db = new NineTapDb())
+            {
+                return (from p in db.Participants
+                    join m in db.Members on p.Member.Id equals m.Id
+                    join g in db.Games on p.Game.Id equals g.Id
+                    join t in db.Tournaments on p.Tournament.Id equals t.Id
+                    where t.Id == selectedTournament
+                    orderby (g.Game1 + g.Game2 + g.Game3 + g.Game4) descending
+                    select new TournamentStatsList
+                    {
+                        Id = p.Member.Number,
+                        FirstName = p.Member.FirstName,
+                        LastName = p.Member.LastName,
+                        Squad = p.Squad,
+                        ScratchTotal = (
+                            (g.Game1.HasValue ? g.Game1 : 0) +
+                            (g.Game2.HasValue ? g.Game2 : 0) +
+                            (g.Game3.HasValue ? g.Game3 : 0) +
+                            (g.Game4.HasValue ? g.Game4 : 0)
+                        ),
+                        Top3Scores = (
+                            // Scratch Total
+                            (g.Game1.HasValue ? g.Game1 : 0) +
+                            (g.Game2.HasValue ? g.Game2 : 0) +
+                            (g.Game3.HasValue ? g.Game3 : 0) +
+                            (g.Game4.HasValue ? g.Game4 : 0)) +
+                            // Handicap
+                            (p.Game.Handicap * 3) +
+                            // Bonus
+                            (p.Game.Bonus * 3),
 
+                        /* It dosent even use game total, why does this exist
+                        GameTotal = (
+                            ((g.Game1.HasValue ? g.Game1 : 0) + (g.Handicap + g.Bonus)) + 
+                            ((g.Game2.HasValue ? g.Game2 : 0) + (g.Handicap + g.Bonus)) + 
+                            ((g.Game3.HasValue ? g.Game3 : 0) + (g.Handicap + g.Bonus)) + 
+                            ((g.Game4.HasValue ? g.Game4 : 0) + (g.Handicap + g.Bonus))
+                        ),
+                        */
+                        Game1 = g.Game1,
+                        Game2 = g.Game2,
+                        Game3 = g.Game3,
+                        Game4 = g.Game4,
+                        Handicap = p.Game.Handicap,
+                        Bonus = p.Game.Bonus,
+                    }).ToList();
+            }
+        }
     }
 }
--- NineTapTour/NineTapTour/Forms/TournamentStats.cs
+++ NineTapTour/NineTapTour/Forms/TournamentStats.cs
@@ -40,55 +40,11 @@ namespace NineTapTour.Forms
                 selectedTournament = frmMemberScores.selectedTournament;
                 lblTournamentName.Text = "Tournament ID: (" + selectedTournament.Id + ")\nTournament Location: " + selectedTournament.Location + "\nDate: " + selectedTournament.Date;
 
-                // query database
-                NineTapDb db = new NineTapDb();
+                // Grabs a list of TournamentStatsList from the database
                 List<TournamentStatsList> statsList = 
-                   (from p in db.Participants
-                    join m in db.Members on p.Member.Id equals m.Id
-                    join g in db.Games on p.Game.Id equals g.Id
-                    join t in db.Tournaments on p.Tournament.Id equals t.Id
-                    where t.Id == selectedTournament.Id
-                    orderby (g.Game1 + g.Game2 + g.Game3 + g.Game4) descending
-                    select new TournamentStatsList
-                    {
-                        Id = p.Member.Number,
-                        FirstName = p.Member.FirstName,
-                        LastName = p.Member.LastName,
-                        Squad = p.Squad,
-                        ScratchTotal = (
-                            (g.Game1.HasValue ? g.Game1 : 0) + 
-                            (g.Game2.HasValue ? g.Game2 : 0) + 
-                            (g.Game3.HasValue ? g.Game3 : 0) + 
-                            (g.Game4.HasValue ? g.Game4 : 0)
-                        ),
-                        Top3Scores = (
-                            // Scratch Total
-                            (g.Game1.HasValue ? g.Game1 : 0) +
-                            (g.Game2.HasValue ? g.Game2 : 0) +
-                            (g.Game3.HasValue ? g.Game3 : 0) +
-                            (g.Game4.HasValue ? g.Game4 : 0)) + 
-                            // Handicap
-                            (p.Game.Handicap * 3) + 
-                            // Bonus
-                            (p.Game.Bonus * 3),
-
-                        /* It dosent even use game total, why does this exist
-                        GameTotal = (
-                            ((g.Game1.HasValue ? g.Game1 : 0) + (g.Handicap + g.Bonus)) + 
-                            ((g.Game2.HasValue ? g.Game2 : 0) + (g.Handicap + g.Bonus)) + 
-                            ((g.Game3.HasValue ? g.Game3 : 0) + (g.Handicap + g.Bonus)) + 
-                            ((g.Game4.HasValue ? g.Game4 : 0) + (g.Handicap + g.Bonus))
-                        ),
-                        */
-                        Game1 = g.Game1,
-                        Game2 = g.Game2,
-                        Game3 = g.Game3,
-                        Game4 = g.Game4,
-                        Handicap = p.Game.Handicap,
-                        Bonus = p.Game.Bonus,
-                    }).ToList();
+                    TournamentStatsListDB.GetTournamentStatsList(selectedTournament.Id);
                 
-                // send to form
+                // Send to form
                 dgvTournamentStats.DataSource = BuildDataTable(statsList);
             }
             else
