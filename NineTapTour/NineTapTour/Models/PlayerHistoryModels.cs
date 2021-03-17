7469e452d767218f1d43a311a1d742744b892ff9
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2021-03-17 08:16:19 -0700

    Refactor GetLeagueAverage method (#979)

---
+++ NineTapTour/NineTapTour/Models/PlayerHistoryModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineTapTour.Models
+{
+    class CurrentHistory
+    {
+        public int ScratchTotal { get; set; }
+
+        public bool UseGame1 { get; set; }
+
+        public bool UseGame2 { get; set; }
+
+        public bool UseGame3 { get; set; }
+
+        public bool UseGame4 { get; set; }
+
+    }
+
+    class PreviousHistory
+    {
+        public DateTime TournamentDate { get; set; }
+
+        public int GamesPlayed { get; set; }
+
+        public int TotalScore { get; set; }
+    }
+}
