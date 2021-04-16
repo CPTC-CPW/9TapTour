004e4799a917fb242f2e74e5962bee00a6600274
Joseph Ortiz <joe@speakinginbits.com>
2021-04-16 14:39:53 -0700

    Comment out product test DB connection string

--- NineTapTour/NineTapTour/Database/NineTapDb.cs
+++ NineTapTour/NineTapTour/Database/NineTapDb.cs
@@ -28,9 +28,9 @@ namespace NineTapTour.Database
                 optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;database=NineTapTour.NineTapDb;trusted_connection=true;MultipleActiveResultSets=true");
 #endif
 
-#if !DEBUG
-                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;database=NineTapTour.NineTapDb;trusted_connection=true;MultipleActiveResultSets=true");
-#endif
+//#if !DEBUG
+//                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;database=NineTapTour.NineTapDb;trusted_connection=true;MultipleActiveResultSets=true");
+//#endif
             }
         }
 
