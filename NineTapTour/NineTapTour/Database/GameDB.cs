99bd8f3106c36a7dd66c8d89937b90b3eae62f2b
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2021-04-20 19:13:12 -0700

    Dispose of DbContext

--- NineTapTour/NineTapTour/Database/GameDB.cs
+++ NineTapTour/NineTapTour/Database/GameDB.cs
@@ -43,13 +43,15 @@ namespace NineTapTour.Database
         /// <param name="games">The Games to add or update</param>
         public static void AddOrUpdateSomeGames(List<Game> games)
         {
-            var db = new NineTapDb();
-            foreach (var currGame in games)
+            using (var db = new NineTapDb())
             {
-                db.Entry(currGame).State = db.Games.Any(g => g.Id == currGame.Id) ?
-                        EntityState.Modified : EntityState.Added;
+                foreach (var currGame in games)
+                {
+                    db.Entry(currGame).State = db.Games.Any(g => g.Id == currGame.Id) ?
+                            EntityState.Modified : EntityState.Added;
+                }
+                db.SaveChanges();
             }
-            db.SaveChanges();
         }
 
         public static Game GetGameInTournament(int memberID, int tournamentID, int squad)
