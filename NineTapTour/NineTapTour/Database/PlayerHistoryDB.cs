2c084a7d4db9f0ea5449c3ffb20561a127e8bafe
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2021-09-01 13:25:21 -0700

    Fixed spelling and grammar in comment

--- NineTapTour/NineTapTour/Database/PlayerHistoryDB.cs
+++ NineTapTour/NineTapTour/Database/PlayerHistoryDB.cs
@@ -198,13 +198,11 @@ namespace NineTapTour.Database
             const int HOW_MANY = 5;
             using (var db = new NineTapDb())
             {
-                /* will only grab the last 5 where the AVG was adjusted, 
-                  that way the bonus pins cant be affected by bowling in more then one squad */
+                // Will only grab the last 5 PlayerHistories where the AVG was adjusted, 
+                // that way the bonus pins can't be affected by bowling in more than one squad
                 List<PlayerHistory> PlayerHistoryList = 
                     (from h in db.PlayerHistory
                     where h.MemberNumber == memberNum && h.regionID == regionID && h.AVG > 0 
-                    /* Only grabs tournaments where average was determined. 
-                      that way it doest grab history from a diffrent sqaud */
                     orderby h.TournamentDate descending, h.hisID descending
                     select h).Take(HOW_MANY).ToList();
                 return PlayerHistoryList;
