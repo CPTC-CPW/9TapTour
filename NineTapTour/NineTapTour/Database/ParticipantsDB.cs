ab0250f617820de31c8036198d02f146673ef862
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-12-04 14:18:25 -0800

    Refactor scratch standings by tournament

--- NineTapTour/NineTapTour/Database/ParticipantsDB.cs
+++ NineTapTour/NineTapTour/Database/ParticipantsDB.cs
@@ -338,19 +338,46 @@ ORDER BY Score DESC";
         {
             using (NineTapDb db = new NineTapDb())
             {
-                return (from g in (db.Participants.Include(b => b.Member)
+                List<MemberScoresInterim> interimScores = (from g in (db.Participants.Include(b => b.Member)
                             .Include(b => b.Game)
                             .Where(b => b.Tournament.Id == selectedTournament))
                         orderby ((g.Game.Game1) + (g.Game.Game2) + (g.Game.Game3) + (g.Game.Game4)) descending
-                        select new MemberScores {
+                        select new MemberScoresInterim {
                             MemberId = g.Member.Number,
                             FirstName = g.Member.FirstName,
                             LastName = g.Member.LastName,
-                            Score = (g.Game.Game1) + (g.Game.Game2) + (g.Game.Game3) + (g.Game.Game4),
+                            Score = 0,
                             LastPaymentYear = (g.Member.IsLifetimeMember) ? "life " : g.Member.LastPayment.Value.Year.ToString(),
                             Paid = (g.Member.IsLifetimeMember == true || !(g.Member.LastPayment != null && 
                                 (g.Member.LastPayment.Value <= DateTime.Now.AddYears(-1))))
                         }).ToList();
+
+                List<MemberScores> memberScores = new();
+                // Use member interim to manually add up the game scores to avoid trying to add null on the database end
+                // and causing the players score to be null
+                foreach (var memberInterim in interimScores)
+                {
+                    if (memberInterim.Game1Score != null)
+                    {
+                        memberInterim.Score += memberInterim.Game1Score;
+                    }
+                    if (memberInterim.Game2Score != null)
+                    {
+                        memberInterim.Score += memberInterim.Game2Score;
+                    }
+                    if (memberInterim.Game3Score != null)
+                    {
+                        memberInterim.Score += memberInterim.Game3Score;
+                    }
+                    if (memberInterim.Game4Score != null)
+                    {
+                        memberInterim.Score += memberInterim.Game4Score;
+                    }
+
+                    memberScores.Add(memberInterim);
+                }
+
+                return memberScores;
             }
         }
 
