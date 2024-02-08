d8182b7a0369f8a99b8c7586241bbf04b3c83a25
joeprogrammer88 <JoeProgrammer88@users.noreply.github.com>
2024-02-08 13:18:59 -0800

    Enable doubles button

--- NineTapTour/NineTapTour/Forms/frmNewTournament.cs
+++ NineTapTour/NineTapTour/Forms/frmNewTournament.cs
@@ -277,9 +277,6 @@ namespace NineTapTour.Forms
         private void FrmNewTournament_Load(object sender, EventArgs e)
         {
             rdoSingles.Checked = true;
-
-            // False for now while feature is being implemented.
-            rdoDoubles.Enabled = false;
         }
     }
 }
