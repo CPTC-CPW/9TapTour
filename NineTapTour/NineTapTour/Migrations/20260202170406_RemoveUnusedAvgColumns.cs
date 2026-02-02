b0beb25f88b9a8235368333397bd8b8b35247d36
JoeProgrammer88 <joe@speakinginbits.com>
2026-02-02 09:53:15 -0800

    Update database migrations to remove unused average columns

---
+++ NineTapTour/NineTapTour/Migrations/20260202170406_RemoveUnusedAvgColumns.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NineTapTour.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUnusedAvgColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GameAvg",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "InputtedAvg",
+                table: "Games");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GameAvg",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "InputtedAvg",
+                table: "Games",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
