ab759187a528af84525d6f2ea2df53edbce57ae7
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2026-05-10 14:52:00 -0700

    Implement doubles team standings and combine partner scores (#1178)

---
+++ NineTapTour/NineTapTour/Models/TeamMemberScores.cs
@@ -0,0 +1,54 @@
+namespace NineTapTour.Models
+{
+    /// <summary>
+    /// A wrapper class that extends MemberScores to represent a doubles team's combined standings.
+    /// Each instance represents a paired team with combined score and both partners' data.
+    /// </summary>
+    public class TeamMemberScores : MemberScores
+    {
+        /// <summary>
+        /// Gets or sets the first team member's ID.
+        /// </summary>
+        public int Partner1MemberId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first team member's first name.
+        /// </summary>
+        public string Partner1FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first team member's last name.
+        /// </summary>
+        public string Partner1LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second team member's ID.
+        /// </summary>
+        public int Partner2MemberId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second team member's first name.
+        /// </summary>
+        public string Partner2FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second team member's last name.
+        /// </summary>
+        public string Partner2LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first partner's individual score.
+        /// </summary>
+        public int? Partner1Score { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second partner's individual score.
+        /// </summary>
+        public int? Partner2Score { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this is a team record (as opposed to an individual).
+        /// </summary>
+        public bool IsTeam { get; set; } = true;
+    }
+}
