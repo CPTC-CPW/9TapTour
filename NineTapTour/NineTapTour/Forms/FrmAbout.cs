e11a2630e165d5ada2881f3d733933f4273a5290
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2026-06-03 07:53:40 -0700

    Clean up AboutForm

--- NineTapTour/NineTapTour/Forms/FrmAbout.cs
+++ NineTapTour/NineTapTour/Forms/FrmAbout.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
-namespace NineTapTour.Forms {
-    public partial class FrmAbout : Form {
-        public FrmAbout() {
-            InitializeComponent();
-        }
+namespace NineTapTour.Forms; 
+public partial class FrmAbout : Form 
+{
+    public FrmAbout() 
+    {
+        InitializeComponent();
     }
 }
