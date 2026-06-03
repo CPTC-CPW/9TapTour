77bd58aa6935539ac3a11c33b21ffc6d315fc7ab
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2026-06-03 13:59:29 -0700

    Clean up .resx files and remove unused icon resources (#1226)
    * Clean up .resx files and remove unused icon resources
    
    - Updated XML comments in FrmAbout.resx, FrmMainMenu.resx, and FrmMemberData.resx for better schema documentation.
    - Removed assembly references and $this.Icon resources from all three forms.
    
    * Remove duplicate icon
    
    * Remove Microsoft.Data.SqlClient package references
    
    Removed Microsoft.Data.SqlClient version 6.1.3 from both MemberImportData.csproj and NineTapTour.csproj to clean up unused dependencies.
    
    * Update client version to 3.1.6

--- NineTapTour/NineTapTour/Forms/FrmMainMenu.cs
+++ NineTapTour/NineTapTour/Forms/FrmMainMenu.cs
@@ -65,7 +65,7 @@ public partial class FrmMainMenu : Form
         Font drawFont = new("Arial", 12);
         SolidBrush drawBrush = new(Color.White);
         PointF drawPoint = new(10, 2);
-        g.DrawString("Version: 3.1.5", drawFont, drawBrush, drawPoint);
+        g.DrawString("Version: 3.1.6", drawFont, drawBrush, drawPoint);
 #if DEBUG
         drawBrush.Color = Color.Red;
         drawPoint.Y += 16;
